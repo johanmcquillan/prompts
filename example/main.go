3f3d23cd312639b15b5aeb22ad2d1c3a566c3729
Johan McQuillan <johangmcquillan@gmail.com>
2022-03-29 12:07:40 +0100

    fix separators and x/sys dependency

--- example/main.go
+++ example/main.go
@@ -14,15 +14,12 @@ func main() {
 		WithComponent(
 			env.MakeUserComponent().
 				WithFormatter(colors.NewShellFormatter(88).SetBold(true))).
-		WithSeparator().
 		WithComponent(
 			git.MakeGitRelativeDirComponent().
 				WithFormatter(colors.NewShellFormatter(69).SetBold(true))).
-		WithSeparator().
 		WithComponent(
 			git.MakeGitBranchComponent().
 				WithFormatter(colors.NewShellFormatter(90).SetBold(true))).
-		WithSeparator().
 		WithComponent(
 			k8s.MakeK8sContextComponent().
 				WithFormatter(colors.NewShellFormatter(22).SetBold(true))).
